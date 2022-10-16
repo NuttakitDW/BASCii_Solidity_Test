7eebc13909db7ec8378d5f4c5caa376b92bc7b14
nuttakit-vy <56815700+NuttakitDW@users.noreply.github.com>
2022-10-16 23:55:03 +0700

    Update CalculatorFactory1.sol

--- src/CalculatorFactory1.sol
+++ src/CalculatorFactory1.sol
@@ -1,15 +1,25 @@
 pragma solidity ^0.8.0;
 
 contract CalculatorFactory1 {
+
     string public name = "Nuttakit Kundum";
-    uint256 public id = 6130159921;
+    uint public id = 6130159921;
 
     address public owner;
 
     struct Calculator {
         string model;
-        uint256 price;
+        uint price;
     }
+
+    enum Status {
+        undefined,
+        open,
+        lunch,
+        close
+    }
+
+    Status public status;
 
     Calculator[] public calculators;
 
@@ -17,48 +27,43 @@ contract CalculatorFactory1 {
         owner = msg.sender;
     }
 
-    function createCalculator(string memory _model, uint256 _price) public {
+    function createCalculator(string memory _model, uint _price) public {
         calculators.push(Calculator(_model, _price));
     }
 
-    function calculate(
-        string memory _operator,
-        uint256 _x,
-        uint256 _y
-    ) public view returns (uint256) {
-        if (_compareStrings(_operator, "plus")) {
-            return _x + _y;
-        }
-        if (_compareStrings(_operator, "minus")) {
-            return _x - _y;
-        }
-        if (_compareStrings(_operator, "modulo")) {
-            return _x % _y;
-        }
+    function calculate(string memory _operator, uint _x, uint _y) public view returns(uint) {
+        if(_compareStrings(_operator, "plus")) {
+            return _x+_y;
+        } 
+        if(_compareStrings(_operator, "minus")) {
+            return _x-_y;
+        } 
+        if(_compareStrings(_operator, "modulo")) {
+            return _x%_y;
+        } 
     }
 
-    function sum(uint256[] memory listOfNum) public view returns (uint256) {
-        uint256 total = 0;
-        for (uint256 i; i < listOfNum.length; i++) {
-            total += listOfNum[i];
+    function loop(uint[] memory listOfNum) public view returns(uint) {
+        uint total = 0;
+        for(uint i; i<listOfNum.length; i++) {
+            total+=listOfNum[i];
         }
         return total;
     }
 
-    function deleteCalculator(uint256 _index) public {
+    function deleteCalculator(uint _index) public onlyOwner {
         delete calculators[_index];
     }
 
-    function _compareStrings(string memory a, string memory b)
-        internal
-        view
-        returns (bool)
-    {
-        return (keccak256(abi.encodePacked((a))) ==
-            keccak256(abi.encodePacked((b))));
+    function setStatus() public {
+        status = Status.open;
     }
 
-    modifier onlyOwner() {
+    function _compareStrings(string memory a, string memory b) internal view returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
